6636764363532bdc4a3c0040a8b51b6ff2b97966
imangi <imanginanayakkara@gmail.com>
2022-06-22 06:54:42 +0530

    update

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const homePageControllers = require("../controllers/homePageControllers");
 const homePageServices = require("../services/homePageServices");
+const postone = require("../services/postone");
 
 const router = express.Router();
 
 const initWebRoutes = (app) => {
   router.get("/", homePageControllers.getHomePage);
   router.get("/webhook", homePageControllers.getWebhook);
-  router.post("/webhook", homePageServices.postOne);
+  /*router.post("/webhook", homePageServices.postOne);*/
+  router.get("/webhook", postone.getpostone);
   return app.use("/", router);
 };
 
