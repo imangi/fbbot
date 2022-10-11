97fc9c20031e0959ee7a593ca704c0cf6b02cf83
imangi <imanginanayakkara@gmail.com>
2022-10-11 21:14:23 +0530

    update

--- src/services/graphApi.js
+++ src/services/graphApi.js
@@ -15,10 +15,12 @@ const APP_SECRET = process.env.APP_SECRET;
 
 class GraphApi {
   static async callSendApi(requestBody) {
-    let url = new URL(`${API_URL}/${PAGE_ID}/messages?`);
-    url.search = new URLSearchParams({
+    let url = new URL(
+      `${API_URL}/${PAGE_ID}/messages?access_token=${PAGE_ACCESS_TOKEN}`
+    );
+    /* url.search = new URLSearchParams({
       access_token: `${PAGE_ACCESS_TOKEN}`,
-    });
+    });*/
     console.warn("Request body is\n" + JSON.stringify(requestBody));
     let response = await axios.post(url, {
       headers: { "Content-Type": "application/json" },
